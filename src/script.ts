204e8bfc402674b1147c4b0921a31064a64ed790
fonkou <fonkoukamtewi@th-nuernberg.de>
2025-11-21 22:07:11 +0100

    add start files.

--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,14 @@
-console.log("Hello world")
+const form = document.getElementById('form') as HTMLFormElement;
+const type = document.getElementById('type') as HTMLSelectElement;
+const firstName = document.getElementById('firstName') as HTMLInputElement;
+const lastName = document.getElementById('lastName') as HTMLInputElement;
+const address = document.getElementById('address') as HTMLInputElement;
+const country = document.getElementById('country') as HTMLInputElement;
+const town = document.getElementById('town') as HTMLInputElement;
+const zip = document.getElementById('zip') as HTMLInputElement;
+const product = document.getElementById('firstName') as HTMLInputElement;
+const price = document.getElementById('price') as HTMLInputElement;
+const quantity = document.getElementById('quantity') as HTMLInputElement;
+const tva = document.getElementById('tva') as HTMLInputElement;
+
+// Submit Form
